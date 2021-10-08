c2bff9014191092648df1495961dbd3bdf7f89ba
MichaelisTrofficus <miguel.otero.pedrido.1993@gmail.com>
2021-10-08 08:04:38 +0200

    Finish Swarm class

--- pso/swarm.py
+++ pso/swarm.py
@@ -1,6 +1,9 @@
-from typing import Tuple, List, Callable
+from typing import List, Callable
+
+import numpy as np
 
 from pso.particle import Particle
+
 
 class Swarm:
     """
@@ -33,11 +36,35 @@ class Swarm:
         self.dimension = dimension
 
         self.global_position_best = None
+        self.global_position_best_fitness = np.inf
 
-    def _select_informants(self, p: Particle, n_informants: int) -> List[Particle]:
-        pass
+        self.animation_particle_information = dict()
 
-    def run(self, fitness_fun: Callable, n_particles: int, n_informants: int):
+    def _calculate_global_position_best(self, particles: List[Particle]):
+        fitness_arr = [p.get_individual_position_fitness for p in particles]
+        min_index = np.argmin(fitness_arr)
+
+        if fitness_arr[min_index] < self.global_position_best_fitness:
+            self.global_position_best_fitness = fitness_arr[min_index]
+            self.global_position_best = particles[min_index].get_individual_position()
+
+    @staticmethod
+    def _get_informants(p_id: int, particles: List[Particle], n_informants: int) -> List[Particle]:
+        particles.pop(p_id)
+        informants = np.random.choice(particles, size=n_informants, replace=False)
+        return informants
+
+    def _create_animation_data_collector(self, particles: List[Particle]):
+        for p_id, p in enumerate(particles):
+            self.animation_particle_information[str(p_id)] = p.get_individual_position()
+
+    def _add_animation_position(self, p_id: int, particle: Particle):
+        self.animation_particle_information[p_id] = particle.get_individual_position()
+
+    def get_global_position_best(self):
+        return self.global_position_best
+
+    def run(self, max_iter: int, fitness_fun: Callable, n_particles: int, n_informants: int):
 
         data = {
             "fitness_fun": fitness_fun,
@@ -52,6 +79,27 @@ class Swarm:
         }
 
         particles = [Particle.from_dict(data) for _ in range(n_particles)]
+        self._create_animation_data_collector(particles)
+
+        for _ in range(max_iter):
+            # 1. We assess the best position of the swarm
+            self._calculate_global_position_best(particles)
+
+            # 2. Iterate for each particle
+            for p_id, p in enumerate(particles):
+
+                # 2.1 Gather particle information for visualization
+                self._add_animation_position(p_id, p)
+
+                # 2.2 We select informants for each particle
+                informants = self._get_informants(p_id, particles, n_informants)
+
+                # 2.2 Update particle position and velocity
+                p.update_position_velocity(informants, self.get_global_position_best())
+
+
+
+
 
 
 
